ad740e1202a62da8fee6e7a7f906f0a4d5e0dd5d
chenzixin <chenzixine@gmail.com>
2013-03-08 10:28:15 +0800

    update

--- pub.sh
+++ pub.sh
@@ -1,16 +1,2 @@
 #!/bin/sh
-do_commit() {
-    cmd = "git commit -a -m\"$log\""
-    echo $cmd
-    git add .;
-    git commit -a -m"$log"
-    git push origin master;
-}
-
-while [ $# -gt 0 ]
-do
-  case $1 in
-    -commit |-u) shift; log = $1; do_commit; exit 0;;
-  esac
-  shift
-done
+git push origin master
