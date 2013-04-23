656d000cbd99132e369a2aae7486b7453a893fd4
chenzixin <chenzixine@gmail.com>
2013-04-23 18:10:18 +0800

    gjus

--- demo/lazy/ga.js
+++ demo/lazy/ga.js
@@ -6,6 +6,12 @@
   (function() {
     var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    ga.onload = ga.onreadystatechange = function() {
+			var st = ga.readyState;
+			if (st && st != "loaded" && st != "complete") return;
+			alert(3);
+		};
+	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);	
+
     alert(2);
   })();
